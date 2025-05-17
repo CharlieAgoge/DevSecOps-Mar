66f2d05498ca588d559c8db5c728dd3b04010789
CharlieAgoge <154438350+CharlieAgoge@users.noreply.github.com>
2025-05-17 11:30:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform_today" {
-  bucket = "terraform-up-and-running-state-today"
+resource "aws_s3_bucket" "terraform_today_today" {
+  bucket = "terraform-up-and-running-state-today-today"
 }
